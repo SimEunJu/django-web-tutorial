0ba3cc9b503e5a0ae988047457cd4ae2fbf2fc0e
SimEunJu <wndmstla123@gmail.com>
2017-08-13 17:14:33 +0900

    Chapter 3 on ing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from blog.models import Post
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
 # Create your views here.
+class PostLV(ListView):
+    model =Post
+    template_name ='blog/post_all.html'
+    context_object_name ='posts'
+    paginate_by =2
+
+class PostDV(DetailView):
+    model =Post
+
+class PostAV(ArchiveIndexView):
+    model =Post
+    date_field ='modify_date'
+
+class PostYAV(YearArchiveView):
+    model =Post
+    date_field ='modify_date'
+    make_object_list =True
+
+class PostMAV(MonthArchiveView):
+    model =Post
+    date_field ='modify_date'
+
+class PostDAV(DayArchiveView):
+    model =Post
+    date_field ='modify_date'
+
+class PostTAV(TodayArchiveView):
+    model =Post
+    date_field ='modify_date'
